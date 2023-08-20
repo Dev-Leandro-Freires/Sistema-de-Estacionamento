70fbb27f13644965232a62107c21c42be0980272
Leandro Freires <freires.leandro@gmail.com>
2023-08-20 15:19:25 -0300

    Eliminando comentários desnecessários e reduzindo o número de comandos Console.Writeln()

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,6 +1,5 @@
 ﻿using DesafioFundamentos.Models;
 
-// Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 decimal precoInicial = 0;
@@ -13,21 +12,19 @@ precoInicial = Convert.ToDecimal(Console.ReadLine());
 Console.WriteLine("Agora digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
 string opcao = string.Empty;
 bool exibirMenu = true;
 
-// Realiza o loop do menu
 while (exibirMenu)
 {
     Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+    Console.WriteLine("Digite a sua opção:\n" + 
+                      "1 - Cadastrar veículo\n" +
+                      "2 - Remover veículo\n" +
+                      "3 - Listar veículos\n" +
+                      "4 - Encerrar\n"); 
 
     switch (Console.ReadLine())
     {
